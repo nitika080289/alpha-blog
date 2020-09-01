23afd15781926e3bdf7a8cf647946ccb33178d2f
Nitika <nitika@wetransfer.com>
2020-09-02 00:32:26 +0200

    Edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,4 +25,18 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was updated successfully'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
 end
